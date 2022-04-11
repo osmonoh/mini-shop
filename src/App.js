342db941d6b59c18ad137a95bc2b0b3c82d8ce8b
Dusan <dusan.chmelko@yahoo.com>
2022-04-11 20:48:55 +0200

    add header + footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>MINI</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
