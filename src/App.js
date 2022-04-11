3f065ba084b9aa12965b49bd05bc71b0196e5900
Dusan <dusan.chmelko@yahoo.com>
2022-04-11 20:52:53 +0200

    install sass + add style folder and App.scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+import "./style/App.scss";
+
 const App = () => {
   return (
     <div className="App">
