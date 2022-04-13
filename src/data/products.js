626d17c1f7f84295ea350d308c4b4090b2e20845
Dusan <dusan.chmelko@yahoo.com>
2022-04-13 14:08:01 +0200

    add productj.js data + shop page

---
+++ src/data/products.js
@@ -0,0 +1,109 @@
+const products = [
+  {
+    id: 1,
+    name: "TABLE LAMPA",
+    img: "https://www.ikea.com/de/de/images/products/gamlared-tisch-antikbeize-hell-schwarz-gebeizt__0737080_pe740872_s5.jpg?f=xxxs",
+    star: 5,
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 2,
+    name: "POTTERY VASE",
+    img: "https://www.ikea.com/de/de/images/products/foerenlig-vase-rosa__0888464_pe782066_s5.jpg?f=xs",
+    star: 4,
+    price: 79,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 3,
+    name: "ROSE HOLDBACK",
+    img: "https://www.ikea.com/de/de/images/products/brunsta-hemma-haengeleuchte-schwarz__0669228_pe714904_s5.jpg?f=xs",
+    star: 5,
+    price: 150,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 4,
+    name: "BLACK CLOCK",
+    img: "https://www.ikea.com/de/de/images/products/stursk-wanduhr-schwarz__0661139_pe711350_s5.jpg?f=xxxs",
+    star: 4,
+    price: 23,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 5,
+    name: "WOOD TABLE",
+    img: "https://www.ikea.com/de/de/images/products/nordby-tisch-gummibaum__0157063_pe315528_s5.jpg?f=xxxs",
+    star: 5,
+    price: 210,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 6,
+    name: "DECO VASE",
+    img: "https://www.ikea.com/de/de/images/products/sockeraert-vase-weiss__0091175_pe226468_s5.jpg?f=xxxs",
+    star: 5,
+    price: 49,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 7,
+    name: "BASKET WITH HANDLES",
+    img: "https://www.ikea.com/de/de/images/products/lustigkurre-korb-naturfarben-seegras__0922225_pe788093_s5.jpg?f=xxxs",
+    star: 4,
+    price: 19,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 8,
+    name: "PINK CUSHION",
+    img: "https://www.ikea.com/de/de/images/products/kransborre-kissen-hellrosa__0920477_pe787064_s5.jpg?f=xxxs",
+    star: 5,
+    price: 19,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 9,
+    name: "SET-3-TOP",
+    img: "https://www.ikea.com/de/de/images/products/fejka-topfpflanze-kuenstl-topf-3er-set-drinnen-draussen-gruen__0898071_pe782543_s5.jpg?f=xxxs",
+    star: 5,
+    price: 39,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 10,
+    name: "PLANT KABA",
+    img: "https://www.ikea.com/de/de/images/products/kapkrusbaer-ampel-graugruen__0919747_pe789482_s5.jpg?f=xxxs",
+    star: 5,
+    price: 9,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+  {
+    id: 11,
+    name: "PLANT KABA",
+    img: "https://www.ikea.com/de/de/images/products/kapkrusbaer-ampel-graugruen__0919747_pe789482_s5.jpg?f=xxxs",
+    star: 5,
+    price: 9,
+  },
+  {
+    id: 12,
+    name: "PLANT KABA",
+    img: "https://www.ikea.com/de/de/images/products/kapkrusbaer-ampel-graugruen__0919747_pe789482_s5.jpg?f=xxxs",
+    star: 5,
+    price: 9,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+  },
+];
+export default products;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import products from "../data/products.js";
 
 const Shop = () => {
-  return <div>SHOP</div>;
+  return (
+    <div className="shop center-box">
+      {products.map((item) => {
+        return (
+          <Link to="/" className="shop-item" key={item.id}>
+            <div className="shop-item-img">
+              <img src={item.img} alt={item.name} />
+            </div>
+            <h3>{item.name}</h3>
+            <p>{item.price}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Shop;
