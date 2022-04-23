4309483aad74e22ed148ae276206d30a70f8faac
Dusan <dusan.chmelko@yahoo.com>
2022-04-23 22:44:34 +0200

    add cart item btns and their functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,7 +2,35 @@ import React, { useContext } from "react";
 import { MyContext } from "../context/MyContext";
 
 const Cart = () => {
-  const { inCart } = useContext(MyContext);
+  const { inCart, setInCart } = useContext(MyContext);
+
+  const onMinusClick = (product) => {
+    setInCart(
+      inCart.map((el) => {
+        if (el.id === product.id && el.amount > 1) {
+          return { ...el, amount: el.amount - 1 };
+        }
+        return el;
+      })
+    );
+  };
+
+  const onPlusClick = (product) => {
+    setInCart(
+      inCart.map((el) => {
+        if (el.id === product.id && el.amount < 10) {
+          return { ...el, amount: el.amount + 1 };
+        }
+        return el;
+      })
+    );
+  };
+
+  const onRemoveClick = (product) => {
+    setInCart(inCart.filter((el) => el.id !== product.id));
+  };
+
+  console.log(inCart);
 
   return (
     <div>
@@ -11,6 +39,9 @@ const Cart = () => {
         <div key={item.id}>
           <p>{item.name}</p>
           <p>{item.amount}</p>
+          <button onClick={() => onMinusClick(item)}>-</button>
+          <button onClick={() => onPlusClick(item)}>+</button>
+          <button onClick={() => onRemoveClick(item)}>remove</button>
         </div>
       ))}
     </div>
