44590704e2e72b06dffce1a0fc2913ff190812a7
Dusan <dusan.chmelko@yahoo.com>
2022-04-24 18:13:56 +0200

    style cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { MyContext } from "../context/MyContext";
 
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
-  const { inCart, setInCart } = useContext(MyContext);
+  const { inCart, setInCart, setProduct } = useContext(MyContext);
 
   const onMinusClick = (product) => {
     setInCart(
@@ -36,50 +37,87 @@ const Cart = () => {
   console.log(inCart);
 
   return (
-    <div className="cart center-box">
-      <div className="cart-list">
-        {inCart.map((item) => (
-          <div className="cart-list-item" key={item.id}>
-            <div className="cart-list-item-image">
-              <img src={item.img} alt={item.name} />
-            </div>
-            <div className="cart-list-item-content">
-              <div className="firstline">
-                <p>{item.name}</p>
-                <p>&euro;{item.price}</p>
-              </div>
-              <div className="lastline">
-                <div className="lastline-left">
-                  {/* <button
+    <div>
+      {inCart.length ? (
+        <div className="cart center-box">
+          <div className="cart-list">
+            {inCart.map((item) => (
+              <div className="cart-list-item" key={item.id}>
+                <Link
+                  to={`/product/${item.id}`}
+                  className="cart-list-item-image"
+                  onClick={() => setProduct(item)}
+                >
+                  <img src={item.img} alt={item.name} />
+                </Link>
+                <div className="cart-list-item-content">
+                  <div className="firstline">
+                    <p>{item.name}</p>
+                    <p>&euro; {item.price}</p>
+                  </div>
+                  <div className="lastline">
+                    <div className="lastline-left">
+                      {/* <button
                     className="btn-change"
                     onClick={() => onMinusClick(item)}
                   >
                   </button> */}
-                  <AiFillMinusCircle
-                    className="btn-change"
-                    onClick={() => onMinusClick(item)}
-                  />
-                  <p>{item.amount}</p>
-                  {/* <button
+                      <AiFillMinusCircle
+                        className="btn-change"
+                        onClick={() => onMinusClick(item)}
+                      />
+                      <p>{item.amount}</p>
+                      {/* <button
                     className="btn-change"
                     onClick={() => onPlusClick(item)}
                   >
                   </button> */}
-                  <AiFillPlusCircle
-                    className="btn-change"
-                    onClick={() => onPlusClick(item)}
-                  />
-                </div>
-                <div className="lastline-right">
-                  {/* <button onClick={() => onRemoveClick(item)}>remove</button> */}
-                  <MdDelete onClick={() => onRemoveClick(item)} />
+                      <AiFillPlusCircle
+                        className="btn-change"
+                        onClick={() => onPlusClick(item)}
+                      />
+                    </div>
+                    <div className="lastline-right">
+                      {/* <button onClick={() => onRemoveClick(item)}>remove</button> */}
+                      <MdDelete onClick={() => onRemoveClick(item)} />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="cart-total"></div>
+
+          <div className="cart-total">
+            <p className="cart-total-amount">
+              <span>Amount</span>
+              <span>
+                {inCart.reduce((acc, item) => (acc += item.amount), 0)}
+              </span>
+            </p>
+            <p className="cart-total-total">
+              <span>Total</span>
+              <span>
+                &euro;{" "}
+                {inCart.reduce(
+                  (acc, item) => (acc += item.price * item.amount),
+                  0
+                )}
+              </span>
+            </p>
+            <p className="cart-total-disclaimer">
+              *This total does not include delivery costs
+            </p>
+            <div className="cart-total-btn">Check out</div>
+          </div>
+        </div>
+      ) : (
+        <div className="cart-empty center-box">
+          <h2 className="cart-empty-heading">Your cart is empty!</h2>
+          <Link to="/shop" className="cart-empty-btn">
+            Back to shop
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
