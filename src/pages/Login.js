26d633d070bb0fe2b27a9fedc7069fa7a2263ec5
Dusan <dusan.chmelko@yahoo.com>
2022-04-28 17:13:00 +0200

    make password visibility toggle

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+
 const Login = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="login center-box">
       <h2>Login</h2>
 
       <form className="login-form" onSubmit={(e) => e.preventDefault()}>
         <input type="email" placeholder="Email*" required />
-        <input type="password" placeholder="Password*" required />
+        <div className="password-input">
+          <input
+            type={visible ? "text" : "password"}
+            placeholder="Password*"
+            required
+          />
+          <div
+            className="password-input-icon"
+            onClick={() => {
+              setVisible(!visible);
+            }}
+          >
+            {visible ? <IoMdEye /> : <IoMdEyeOff />}
+          </div>
+        </div>
         <p>
           You don't have an account yet? Please register{" "}
           <Link to="/register">here</Link>.
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+
 const Register = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="register center-box">
       <h2>Register</h2>
@@ -10,7 +14,21 @@ const Register = () => {
         <input type="text" placeholder="First name*" required />
         <input type="text" placeholder="Last name*" required />
         <input type="email" placeholder="Email*" required />
-        <input type="password" placeholder="Password*" required />
+        <div className="password-input">
+          <input
+            type={visible ? "text" : "password"}
+            placeholder="Password*"
+            required
+          />
+          <div
+            className="password-input-icon"
+            onClick={() => {
+              setVisible(!visible);
+            }}
+          >
+            {visible ? <IoMdEye /> : <IoMdEyeOff />}
+          </div>
+        </div>
         <p>
           You have an account already? Please login{" "}
           <Link to="/login">here</Link>.
